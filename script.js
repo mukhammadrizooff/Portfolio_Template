ee7ca22f662aa16107ed3dc901a2a25c50afda22
Your Name <legend.it.bk.ru>
2021-12-22 00:28:39 +0500

    Update files

--- script.js
+++ script.js
@@ -4,22 +4,6 @@ function main() {
   let hamburguer = document.querySelector("#humburger-btn");
   let body = document.querySelector("body");
   let anchorTags = document.querySelectorAll(".mobile-menu-ul li");
-  /*
-  function openMenuIcon() {
-    menu.classList.add("visible");
-    body.classList.add("overflow-hidden");
-  }
-
-  function closeMenuIcon() {
-    menu.classList.remove("visible");
-    body.classList.remove("overflow-hidden");
-  }
-
-  hamburguer.addEventListener("click", openMenuIcon);
-  closeButton.addEventListener("click", closeMenuIcon);
-  anchorTags.forEach((link) => {
-    link.addEventListener("click", closeMenuIcon);
-  }); */
 }
 
 main();
