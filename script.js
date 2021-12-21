2948ce1428b53aad8c588254eab65c131b76bd70
Your Name <legend.it.bk.ru>
2021-12-22 01:29:09 +0500

    Fix Linters

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function main() {
-  const menu = document.querySelector("#menu-container");
-  const closeButton = document.querySelector(".close-btn");
-  const hamburger = document.querySelector("#humburger-btn");
-  const body = document.querySelector("body");
-  const anchorTags = document.querySelectorAll(".mobile-menu-ul li");
+  const menu = document.querySelector('#menu-container');
+  const closeButton = document.querySelector('.close-btn');
+  const hamburger = document.querySelector('#humburger-btn');
+  const body = document.querySelector('body');
+  const anchorTags = document.querySelectorAll('.mobile-menu-ul li');
 
   function openMenuIcon() {
-    menu.classList.add("visible");
-    body.classList.add("overflow-hidden");
+    menu.classList.add('visible');
+    body.classList.add('overflow-hidden');
   }
 
   function closeMenuIcon() {
-    menu.classList.remove("visible");
-    body.classList.remove("overflow-hidden");
+    menu.classList.remove('visible');
+    body.classList.remove('overflow-hidden');
   }
 
-  hamburger.addEventListener("click", openMenuIcon);
-  closeButton.addEventListener("click", closeMenuIcon);
+  hamburger.addEventListener('click', openMenuIcon);
+  closeButton.addEventListener('click', closeMenuIcon);
   anchorTags.forEach((link) => {
-    link.addEventListener("click", closeMenuIcon);
+    link.addEventListener('click', closeMenuIcon);
   });
 }
 
