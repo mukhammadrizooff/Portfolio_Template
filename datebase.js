1306685d41931a6a835fee47e64dcb755cc50a87
Your Name <legend.it.bk.ru>
2021-12-25 03:30:16 +0500

    Update

--- datebase.js
+++
@@ -1,6 +0,0 @@
-// Variables
-
-const emailId = formStorage.querySelector('#email');
-const msgId = formStorage.querySelector('#invalid-msg');
-const formId = document.querySelector('#form');
-const nameId = formStorage.querySelector('#username');
